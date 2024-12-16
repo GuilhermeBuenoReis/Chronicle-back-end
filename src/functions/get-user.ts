82d73dec9ace790a5bfeaa6af0670a423e4267ba
Guilherme Bueno dos Reis <guilhermebuenoreis@gmail.com>
2024-12-16 15:35:16 -0300

    feat: criação de factories para testes, e setup de vitest para teste unitários

--- src/functions/get-user.ts
+++ src/functions/get-user.ts
@@ -5,12 +5,13 @@ import { users } from '../db/schema';
 interface GetUserRequest {
   userId: string;
 }
+
 export async function getUser({ userId }: GetUserRequest) {
   const result = await db
     .select({
       id: users.id,
-      name: users.id,
-      email: users.id,
+      name: users.name,
+      email: users.email,
       avatarUrl: users.avatarUrl,
     })
     .from(users)
